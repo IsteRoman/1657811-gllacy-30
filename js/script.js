0babb2d3d16878e420801964883b12a86b587c86
IsteRoman <demonatr@mail.ru>
2021-02-24 22:27:40 +0500

    допил js

--- js/script.js
+++ js/script.js
@@ -10,45 +10,56 @@ const third_slide = document.querySelector('.third-slider');
 const third = document.querySelector('.third');
 const open_form = document.querySelector('.button-open-modal');
 const modal_form = document.querySelector('.modal');
-const overlay_block =document.querySelector('.overlay');
+const overlay_block = document.querySelector('.overlay');
+const form = modal_form.querySelector('.modal-form');
+const login = modal_form.querySelector('[name=modal-user-name]');
+const email = modal_form.querySelector('[name=modal-user-email]');
 const close_form = document.querySelector('.close');
 
 if (sliders_block) {
-  const background_clear = function() {
+  const backgroundClear = function() {
     page_background.classList.remove('background-color-first');
     page_background.classList.remove('background-color-second');
     page_background.classList.remove('background-color-third');
   }
-  const sliders_clear = function() {
+
+  const slidersClear = function() {
     for (let i = 0; i<sliders.length; i++) {
       sliders[i].classList.add('element-hidden');
     }
   }
-  const button_clear = function() {
+
+  const buttonClear = function() {
     for (let i = 0; i<slide_buttons.length; i++) {
       slide_buttons[i].classList.remove('slider-button-active');
     }
   }
-  first.addEventListener('click', function() {
-    background_clear();
-    sliders_clear();
-    button_clear();
+
+  first.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    backgroundClear();
+    slidersClear();
+    buttonClear();
     first_slide.classList.remove('element-hidden');
     first.classList.add('slider-button-active');
     page_background.classList.add('background-color-first');
   });
-  second.addEventListener('click', function() {
-    background_clear();
-    sliders_clear();
-    button_clear();
+
+  second.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    backgroundClear();
+    slidersClear();
+    buttonClear();
     second_slide.classList.remove('element-hidden');
     second.classList.add('slider-button-active');
     page_background.classList.add('background-color-second');
   });
-  third.addEventListener('click', function() {
-    background_clear();
-    sliders_clear();
-    button_clear();
+
+  third.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    backgroundClear();
+    slidersClear();
+    buttonClear();
     third_slide.classList.remove('element-hidden');
     third.classList.add('slider-button-active');
     page_background.classList.add('background-color-third');
@@ -56,13 +67,72 @@ if (sliders_block) {
 }
 
 if (modal_form) {
-  open_form.addEventListener('click', function() {
-    modal_form.classList.remove('element-hidden');
-    overlay_block.classList.remove('element-hidden');
-  });
-
-  close_form.addEventListener('click', function() {
+  const closeModal = function() {
+    modal_form.classList.remove('modal-appearance');
+    overlay_block.classList.remove('overlay-appearance');
+    modal_form.classList.remove('modal-error');
     modal_form.classList.add('element-hidden');
     overlay_block.classList.add('element-hidden');
+  }
+
+  let isStorageSupport = true;
+  const storage = '';
+
+  try {
+    storage = localStorage.getItem('login');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  open_form.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modal_form.classList.remove('modal-disappeared');
+    modal_form.classList.remove('element-hidden');
+    modal_form.classList.add('modal-appearance');
+    overlay_block.classList.remove('overlay-disappeared');
+    overlay_block.classList.remove('element-hidden');
+    overlay_block.classList.add('overlay-appearance');
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+  });
+
+  close_form.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modal_form.classList.add('modal-disappeared');
+    overlay_block.classList.add('overlay-disappeared');
+    setTimeout (closeModal, 1000);
+  });
+
+  form.addEventListener('submit', function(evt) {
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+      modal_form.classList.remove('modal-error');
+      modal_form.offsetWidth = modal_form.offsetWidth;
+      modal_form.classList.add('modal-error');
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem('login', login.value);
+      }
+    }
+  });
+
+  window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      modal_form.classList.add('modal-disappeared');
+      overlay_block.classList.add('overlay-disappeared');
+      setTimeout (closeModal, 1000);
+    }
+  });
+
+  overlay_block.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modal_form.classList.add('modal-disappeared');
+    overlay_block.classList.add('overlay-disappeared');
+    setTimeout (closeModal, 1000);
   });
 }
